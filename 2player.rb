a4b0916d9ad06c5fe469d62c0c5e9a6ec5160fb0
jenniferhsueh <jenhsueh89@hotmail.com>
2018-06-19 04:55:30 +0000

    initial commit

---
+++ 2player.rb
@@ -0,0 +1,24 @@
+class Player
+
+  def initialize(name)
+    @name = name
+  end
+ 
+end
+
+class Life
+
+  def intialize(lives)
+    @lives = 3
+  end
+end
+
+class Answer
+end
+
+class Question
+end
+
+class TUrn
+end
+
---
+++ main.rb
@@ -0,0 +1,3 @@
+require './players'
+require './questions'
+require './filters'
